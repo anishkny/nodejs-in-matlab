5aaa2f365f9a7212e6f508b2988c68fb8e5dab96
Anish Karandikar <anishkny@gmail.com>
2018-10-01 20:21:33 -0400

    docs(puppeteer): Take screenshot from ThingSpeak channel

--- examples/puppeteer/index.js
+++ examples/puppeteer/index.js
@@ -3,8 +3,17 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.setViewport({
+    width: 1600,
+    height: 900,
+  });
+  await page.goto('https://thingspeak.com/channels/12397', {
+    'waitUntil': 'networkidle0',
+  });
+  await page.screenshot({
+    path: 'example.png',
+    fullPage: true,
+  });
 
   await browser.close();
 })();
